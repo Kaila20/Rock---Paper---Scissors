ea57713cb53889f4f53594a5dc1cdbc91ab96fda
Kaila <87931787+Kaila20@users.noreply.github.com>
2024-07-16 17:25:13 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//Creating a new object
+const person = new Object(); 
+
+/*Object */ person.FirstName = "John"; //Properties
+person.LastName = "Doe";
+person.Age = 50;
+person.EyeColor = "blue";
+
+//Display Object Content
+document.getElementById("demo").innerHTML = person.FirstName + " Is " + person.Age + " years old.";
+
+
+//creating a new object
+const person2 = new Object();
+
+/* Object */ person2.FirstName = "Kaila"
